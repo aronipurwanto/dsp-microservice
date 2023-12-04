a11a5f8bfa0557bb5092fc7bd73900553387d160
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-12-04 11:36:21 +0700

    feat: cyclic dependecy

--- belajar-spring/src/main/java/com/dsp/microservice/config/DependencyInjectionConfiguration.java
+++ belajar-spring/src/main/java/com/dsp/microservice/config/DependencyInjectionConfiguration.java
@@ -6,11 +6,18 @@ import com.dsp.microservice.model.FooBar;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class DependencyInjectionConfiguration {
     @Bean(value = "fooFirst")
-    public Foo foo(){
+    public Foo foo1(){
+        return new Foo();
+    }
+
+    @Primary
+    @Bean(value = "fooSecond")
+    public Foo foo2(){
         return new Foo();
     }
 
