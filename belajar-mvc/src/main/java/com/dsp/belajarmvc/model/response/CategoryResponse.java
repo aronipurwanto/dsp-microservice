aca9642499d5f7f308133f15180ac80234eff244
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-12-05 10:38:23 +0700

    feat: category

--- belajar-mvc/src/main/java/com/dsp/belajarmvc/model/response/CategoryResponse.java
+++ belajar-mvc/src/main/java/com/dsp/belajarmvc/model/response/CategoryResponse.java
@@ -1,4 +1,15 @@
 package com.dsp.belajarmvc.model.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryResponse {
+    private Long id;
+    private String code;
+    private String name;
+    private String description;
 }
