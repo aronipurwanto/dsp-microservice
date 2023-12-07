2c35b7de167b7a7ea1d9d38cf5a138a5e646d168
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-12-07 10:07:30 +0700

    feat: controller register

--- belajar-restapi/src/main/java/com/dsp/restapi/controller/UserController.java
+++ belajar-restapi/src/main/java/com/dsp/restapi/controller/UserController.java
@@ -1,9 +1,33 @@
 package com.dsp.restapi.controller;
 
+import com.dsp.restapi.model.request.RegisterRequest;
+import com.dsp.restapi.model.response.Response;
+import com.dsp.restapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+    private UserService service;
+
+    @Autowired
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody RegisterRequest request){
+        Response result = service.register(request).orElse(null);
+        if(result == null){
+            return ResponseEntity.ok(null);
+        }
+        return new ResponseEntity<>(result,
+                HttpStatus.valueOf(result.getStatusCode()));
+    }
 }
