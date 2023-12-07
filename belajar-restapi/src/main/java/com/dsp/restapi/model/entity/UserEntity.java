c2c426c37296a9c9401f30650d3c9f1d55a8bc75
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-12-07 09:15:47 +0700

    feat: rest api

--- belajar-restapi/src/main/java/com/dsp/restapi/model/entity/UserEntity.java
+++ belajar-restapi/src/main/java/com/dsp/restapi/model/entity/UserEntity.java
@@ -1,4 +1,34 @@
 package com.dsp.restapi.model.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigInteger;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tbl_users")
 public class UserEntity {
+    @Id
+    @Column(name = "id", length = 36, unique = true)
+    private String id;
+    @Column(name = "username", length = 100, nullable = false)
+    private String username;
+    @Column(name = "email", length = 100, nullable = false)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "token")
+    private String token;
+    @Column(name = "token_expired")
+    private BigInteger tokenExpired;
 }
