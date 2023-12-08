2b10a30c8e01c4b5b974164ff31f741eb0ac078b
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2023-12-08 10:54:45 +0700

    feat: department complete

--- belajar-microservice/department-service/src/main/java/com/dsp/departmentservice/controller/DepartmentController.java
+++ belajar-microservice/department-service/src/main/java/com/dsp/departmentservice/controller/DepartmentController.java
@@ -1,9 +1,79 @@
 package com.dsp.departmentservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.dsp.departmentservice.model.request.DepartmentRequest;
+import com.dsp.departmentservice.model.response.Response;
+import com.dsp.departmentservice.service.DepartmentService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/department/")
+@RequestMapping("/department")
 public class DepartmentController {
+    private DepartmentService service;
+
+    @Autowired
+    public DepartmentController(DepartmentService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<Response> getAll(){
+        var result = this.service.getAll();
+        return ResponseEntity.ok(
+                new Response(200, "Success", result)
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Response> getById(@PathVariable("id") String id){
+        var result = this.service.getById(id);
+        return ResponseEntity.ok(
+                new Response(200, "Success", result)
+        );
+    }
+
+    @PostMapping
+    public ResponseEntity<Response> create(
+            @RequestBody @Valid DepartmentRequest request
+            ){
+        var result = this.service.save(request).orElse(null);
+        if(result == null){
+            return ResponseEntity.ok(
+                    new Response(500, "Failed", null)
+            );
+        }
+        return ResponseEntity.ok(
+                new Response(200, "Success", result)
+        );
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Response> update(
+            @PathVariable("id") String id,
+            @RequestBody @Valid DepartmentRequest request){
+        var result = this.service.update(id,request).orElse(null);
+        if(result == null){
+            return ResponseEntity.ok(
+                    new Response(500, "Failed", null)
+            );
+        }
+        return ResponseEntity.ok(
+                new Response(200, "Success", result)
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Response> delete(@PathVariable("id") String id){
+        var result = this.service.delete(id).orElse(null);
+        if(result == null){
+            return ResponseEntity.ok(
+                    new Response(500, "Failed", null)
+            );
+        }
+        return ResponseEntity.ok(
+                new Response(200, "Success", result)
+        );
+    }
 }
